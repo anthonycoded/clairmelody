3441d7d18642fe97c54b4e6bb6d70b2a6e71fb97
shawn barbel <barbels33@gmail.com>
2021-10-23 11:34:19 -0400

    created blank beat screen

---
+++ src/screens/BeatsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const BeatsScreen = () => {
+  return (
+    <View>
+      <Text>Beats Scren</Text>
+    </View>
+  );
+};
+
+export default BeatsScreen;
