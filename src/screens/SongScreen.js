d8a345f51b88f620ff4a48f4c1739d40143c15f0
shawn barbel <barbels33@gmail.com>
2021-10-23 11:34:28 -0400

    created blank song screen

---
+++ src/screens/SongScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SongScreen = () => {
+  return (
+    <View>
+      <Text>Song Scren</Text>
+    </View>
+  );
+};
+
+export default SongScreen;
