587c83cff24321e037eb01f17ae105a178f0f46e
shawn barbel <barbels33@gmail.com>
2021-10-23 11:50:25 -0400

    created songs reducr with dummy songs

---
+++ src/store/reducers/SongsReducer.js
@@ -0,0 +1,48 @@
+initialState = [
+  {
+    id: 1,
+    title: "Summer Time",
+    duration: "",
+    image:
+      "https://storage.googleapis.com/beatdealer-images/0eb8d9cf-1f70-46da-8ae8-b3e5331ca474",
+    audioUrl:
+      "https://storage.googleapis.com/beatdealer-beats/(FREE)%20xxxtentacion%20type%20beat%20mess.mp3.mpga",
+  },
+  {
+    id: 1,
+    title: "Summer Time",
+    duration: "",
+    image:
+      "https://storage.googleapis.com/beatdealer-images/0eb8d9cf-1f70-46da-8ae8-b3e5331ca474",
+    audioUrl:
+      "https://storage.googleapis.com/beatdealer-beats/(FREE)%20xxxtentacion%20type%20beat%20mess.mp3.mpga",
+  },
+  {
+    id: 1,
+    title: "Summer Time",
+    duration: "",
+    image:
+      "https://storage.googleapis.com/beatdealer-images/0eb8d9cf-1f70-46da-8ae8-b3e5331ca474",
+    audioUrl:
+      "https://storage.googleapis.com/beatdealer-beats/(FREE)%20xxxtentacion%20type%20beat%20mess.mp3.mpga",
+  },
+  {
+    id: 1,
+    title: "Summer Time",
+    duration: "",
+    image:
+      "https://storage.googleapis.com/beatdealer-images/0eb8d9cf-1f70-46da-8ae8-b3e5331ca474",
+    audioUrl:
+      "https://storage.googleapis.com/beatdealer-beats/(FREE)%20xxxtentacion%20type%20beat%20mess.mp3.mpga",
+  },
+];
+
+export const SongsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case New_Merchant:
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+};
