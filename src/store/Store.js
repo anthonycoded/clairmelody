513a3e1819ef45528312d8d2f1f015ceecce9232
shawn barbel <barbels33@gmail.com>
2021-10-23 11:50:37 -0400

    added song reducer to store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from "redux";
 import { MainReducer } from "./reducers/MainReducer";
+import { SongsReducer } from "./reducers/SongsReducer";
 
 const rootReducer = combineReducers({
   main: MainReducer,
+  songs: SongsReducer,
 });
 
 const store = createStore(rootReducer);
