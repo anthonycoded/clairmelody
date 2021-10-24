c54b10a38e437aa91c9d66bd4057e55e51850dea
shawn barbel <barbels33@gmail.com>
2021-10-24 09:53:49 -0400

    added, image, play btn, and styles

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { config } from "../config/Config";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+const url =
+  "https://storage.googleapis.com/beatdealer-images/0eb8d9cf-1f70-46da-8ae8-b3e5331ca474";
 
 const Player = () => {
   return (
-    <View style={style.container}>
-      <Text>Media PLayer </Text>
+    <View style={styles.container}>
+      <Image source={{ uri: url }} style={styles.image} />
+      <Text style={styles.title}>Media PLayer </Text>
+      <TouchableOpacity style={{ width: 50 }}>
+        <FontAwesome5 name="play" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     backgroundColor: "red",
     height: config.hp("8%"),
@@ -18,6 +26,17 @@ const style = StyleSheet.create({
     width: "100%",
     bottom: 80,
     flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: config.wp("2%"),
+  },
+  image: {
+    height: 60,
+    width: 60,
+  },
+  title: {
+    fontSize: 20,
   },
 });
 
