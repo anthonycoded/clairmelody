81a84c71efef4b490b17205586a16e59f813aaec
shawn barbel <barbels33@gmail.com>
2021-10-24 09:42:38 -0400

    added player

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -11,136 +11,140 @@ import { theme } from "../config/Theme";
 import HomeNavigator from "./MainNavigator";
 import SongScreen from "../screens/SongScreen";
 import BeatScreen from "../screens/BeatsScreen";
+import Player from "../components/Player";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        activeTintColor: theme.colors.primary,
-        activeBackgroundColor: theme.colors.primary,
-        inactiveTintColor: theme.colors.inActive,
-        tabStyle: {
-          backgroundColor: "#FFFFFF",
-        },
-        labelStyle: {
-          fontSize: config.hp("1.75%"),
-        },
-        keyboardHidesTabBar: true,
-        headerShown: false,
-      }}
-    >
-      <Tab.Screen
-        name="homeTab"
-        component={HomeNavigator}
-        options={{
-          tabBarLabel: ({ focused, color }) => (
-            <Text
-              style={{
-                color: focused ? theme.colors.primary : theme.colors.inActive,
-                fontSize: 14,
-              }}
-            >
-              Home
-            </Text>
-          ),
-          tabBarColor: theme.colors.primary,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="home-outline"
-              size={focused ? 24 : 22}
-              color={focused ? theme.colors.primary : theme.colors.inActive}
-              backgroundColor={focused ? theme.colors.primary : "none"}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="SongsScreen"
-        component={SongScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: ({ focused, color }) => (
-            <Text
-              style={{
-                color: focused ? theme.colors.primary : theme.colors.inActive,
-                fontSize: 14,
-              }}
-            >
-              Songs
-            </Text>
-          ),
-          tabBarColor: theme.colors.primary,
-          tabBarIcon: ({ focused }) => (
-            <FontAwesome5
-              name="headphones"
-              size={24}
-              color={focused ? theme.colors.primary : theme.colors.inActive}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Beats"
-        component={BeatScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: ({ focused, color }) => (
-            <Text
-              style={{
-                color: focused ? theme.colors.primary : theme.colors.inActive,
-                fontSize: 14,
-              }}
-            >
-              Beats
-            </Text>
-          ),
-          tabBarColor: theme.colors.primary,
-          tabBarIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name="piano"
-              size={24}
-              color={focused ? theme.colors.primary : theme.colors.inActive}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="blank"
-        component={HomeNavigator}
-        options={{
-          tabBarLabel: "Menu",
-          tabBarColor: "black",
-          tabBarLabel: ({ focused }) => (
-            <Text
-              style={{
-                color: focused ? theme.colors.primary : theme.colors.inActive,
-                fontSize: 14,
-              }}
-            >
-              Menu
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="menu-outline"
-              size={focused ? 24 : 22}
-              color={focused ? theme.colors.primary : theme.colors.inActive}
-              backgroundColor={focused ? theme.colors.primary : "none"}
-            />
-          ),
-        }}
-        listeners={({ navigation }) => ({
-          tabPress: (e) => {
-            e.preventDefault();
-            navigation.openDrawer();
+    <>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          activeTintColor: theme.colors.primary,
+          activeBackgroundColor: theme.colors.primary,
+          inactiveTintColor: theme.colors.inActive,
+          tabStyle: {
+            backgroundColor: "#FFFFFF",
           },
-        })}
-      />
-    </Tab.Navigator>
+          labelStyle: {
+            fontSize: config.hp("1.75%"),
+          },
+          keyboardHidesTabBar: true,
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name="homeTab"
+          component={HomeNavigator}
+          options={{
+            tabBarLabel: ({ focused, color }) => (
+              <Text
+                style={{
+                  color: focused ? theme.colors.primary : theme.colors.inActive,
+                  fontSize: 14,
+                }}
+              >
+                Home
+              </Text>
+            ),
+            tabBarColor: theme.colors.primary,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="home-outline"
+                size={focused ? 24 : 22}
+                color={focused ? theme.colors.primary : theme.colors.inActive}
+                backgroundColor={focused ? theme.colors.primary : "none"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="SongsScreen"
+          component={SongScreen}
+          options={{
+            headerShown: false,
+            tabBarLabel: ({ focused, color }) => (
+              <Text
+                style={{
+                  color: focused ? theme.colors.primary : theme.colors.inActive,
+                  fontSize: 14,
+                }}
+              >
+                Songs
+              </Text>
+            ),
+            tabBarColor: theme.colors.primary,
+            tabBarIcon: ({ focused }) => (
+              <FontAwesome5
+                name="headphones"
+                size={24}
+                color={focused ? theme.colors.primary : theme.colors.inActive}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Beats"
+          component={BeatScreen}
+          options={{
+            headerShown: false,
+            tabBarLabel: ({ focused, color }) => (
+              <Text
+                style={{
+                  color: focused ? theme.colors.primary : theme.colors.inActive,
+                  fontSize: 14,
+                }}
+              >
+                Beats
+              </Text>
+            ),
+            tabBarColor: theme.colors.primary,
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name="piano"
+                size={24}
+                color={focused ? theme.colors.primary : theme.colors.inActive}
+              />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="blank"
+          component={HomeNavigator}
+          options={{
+            tabBarLabel: "Menu",
+            tabBarColor: "black",
+            tabBarLabel: ({ focused }) => (
+              <Text
+                style={{
+                  color: focused ? theme.colors.primary : theme.colors.inActive,
+                  fontSize: 14,
+                }}
+              >
+                Menu
+              </Text>
+            ),
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="menu-outline"
+                size={focused ? 24 : 22}
+                color={focused ? theme.colors.primary : theme.colors.inActive}
+                backgroundColor={focused ? theme.colors.primary : "none"}
+              />
+            ),
+          }}
+          listeners={({ navigation }) => ({
+            tabPress: (e) => {
+              e.preventDefault();
+              navigation.openDrawer();
+            },
+          })}
+        />
+      </Tab.Navigator>
+      <Player></Player>
+    </>
   );
 };
 
