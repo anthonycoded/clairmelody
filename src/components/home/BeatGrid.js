c59c845fa6ffc62b855ee1e7f1b4c0f6d221bc78
anthony <barbels33@gmail.com>
2021-11-24 12:59:57 -0500

    created beat grid sliders

---
+++ src/components/home/BeatGrid.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import BeatCard from "./BeatCard";
+import { theme } from "../../config/Theme";
+import { config } from "../../config/Config";
+
+const BeatGrid = () => {
+  const beats = useSelector((state) => state.beats);
+  const data = useSelector((state) => state.songs);
+
+  const renderItem = ({ item }) => <BeatCard item={item}></BeatCard>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Trending Beats</Text>
+      <FlatList
+        data={beats}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        horizontal
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        horizontal
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: config.hp("4%"),
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: theme.colors.primary,
+    fontWeight: "bold",
+    paddingVertical: config.hp("1%"),
+    paddingVertical: config.hp("1%"),
+  },
+});
+
+export default BeatGrid;
