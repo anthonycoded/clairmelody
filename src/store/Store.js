df87776deccd7260748aa6c595a7b8693a9d8b73
anthony <barbels33@gmail.com>
2021-11-24 12:58:24 -0500

    removed unused code

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { MainReducer } from "./reducers/MainReducer";
 import { SongsReducer } from "./reducers/SongsReducer";
 import { PlayerReducer } from "./reducers/PlayerReducer";
 import { BeatsReducers } from "./reducers/BeatsReducer";
 
 const rootReducer = combineReducers({
-  main: MainReducer,
   songs: SongsReducer,
   player: PlayerReducer,
   beats: BeatsReducers,
