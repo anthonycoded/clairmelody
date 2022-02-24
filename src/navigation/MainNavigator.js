47be350c606e58e87a2f7d3301551d26f4891d52
shawn barbel <barbels33@gmail.com>
2022-02-23 20:10:38 -0500

    restructured navigation

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,13 +1,33 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import HomeScreen from "../screens/HomeScreen";
+import ProfileSettingsNavigator from "./ProfileSettingsNavigator";
+import GoBack from "../components/GoBack";
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+          headerBackVisible: false,
+        })}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="ProfileNavigator"
+        component={ProfileSettingsNavigator}
+        options={({ navigation }) => ({
+          headerBackVisible: false,
+
+          headerLeft: () => {
+            return <GoBack navigation={navigation} />;
+          },
+        })}
+      ></Stack.Screen>
     </Stack.Navigator>
   );
 };
