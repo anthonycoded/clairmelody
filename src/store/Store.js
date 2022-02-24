6190e81bd2dbde82efa37dde49cee50964726630
shawn barbel <barbels33@gmail.com>
2022-02-23 20:16:48 -0500

    added auth reducer

--- src/store/Store.js
+++ src/store/Store.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import { songReducer } from "./reducers/songReducer";
 import { PlayerReducer } from "./reducers/PlayerReducer";
 import { beatReducer } from "./reducers/beatReducer";
+import { authReducer } from "./reducers/authReducer";
 
 const rootReducer = combineReducers({
   songs: songReducer,
   player: PlayerReducer,
   beats: beatReducer,
+  auth: authReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
