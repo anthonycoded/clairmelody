c7f9b36386e40425a7e3caa863cb353866216d96
shawn barbel <barbels33@gmail.com>
2022-03-05 16:05:08 -0500

    added actions

--- src/store/actions/PlayerActions.js
+++ src/store/actions/PlayerActions.js
@@ -1,4 +1,6 @@
 const Select_Track = "Select_Track";
+const Update_State = "Update_State";
+const Set_Play_List = "Set_Play_List";
 
 export const SelectTrack = (payload) => async (dispatch) => {
   try {
@@ -12,6 +14,24 @@ export const GetTracks = (payload) => async (dispatch) => {
   try {
     console.log("Action payload", payload);
     dispatch({ type: Select_Track, payload: payload });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const UpdateState = (payload) => async (dispatch) => {
+  try {
+    console.log("Action payload", payload);
+    dispatch({ type: Update_State, payload: payload });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const SetPlayList = (payload) => async (dispatch) => {
+  try {
+    console.log("Action payload", payload);
+    dispatch({ type: Set_Play_List, payload: payload });
   } catch (error) {
     console.log(error);
   }
