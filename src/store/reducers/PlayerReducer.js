9b9c473db9149c8ad6f98a890e8f6c36d0880ebb
shawn barbel <barbels33@gmail.com>
2022-03-05 16:05:20 -0500

    added set playlist

--- src/store/reducers/PlayerReducer.js
+++ src/store/reducers/PlayerReducer.js
@@ -1,14 +1,24 @@
 const initialState = {
   currentTrack: undefined,
-  nextTrack: "",
-  prevTrack: "",
+  error: undefined,
+  playlist: [],
 };
 const Select_Track = "Select_Track";
+const Update_State = "Update_State";
+const Set_Play_List = "Set_Play_List";
 
 export const PlayerReducer = (state = initialState, action) => {
   switch (action.type) {
     case Select_Track:
       return { ...state, currentTrack: action.payload };
+    case Update_State:
+      return { ...state, ...action.payload };
+    case Set_Play_List:
+      return {
+        ...state,
+        playlist: action.payload,
+        currentTrack: action.payload[0],
+      };
     default:
       return state;
   }
