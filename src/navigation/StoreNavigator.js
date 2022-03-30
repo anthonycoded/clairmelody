482a5332363c70b3a773a80c16b6c32c86c44439
shawn barbel <barbels33@gmail.com>
2022-03-30 19:59:30 -0400

    added screens

--- src/navigation/StoreNavigator.js
+++ src/navigation/StoreNavigator.js
@@ -1,24 +1,80 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import HomeScreen from "../screens/homeScreen/HomeScreen";
+import Header from "../components/Header";
 import StoreScreen from "../screens/store/StoreScreen";
+import CollectionScreen from "../screens/store/Collections/CollectionScreen";
+import MerchDetailsScreen from "../screens/store/merchandise/MerchDetailsScreen";
+import MusicDetailScreen from "../screens/store/sections/musicDetails/MusicDetailScreen";
 import GoBack from "../components/GoBack";
+import CartScreen from "../screens/store/cart/CartScreen";
 
 const Stack = createNativeStackNavigator();
 
-const MainNavigator = () => {
+const StoreNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator>
       <Stack.Screen
         name="index"
         component={StoreScreen}
         options={({ navigation }) => ({
-          headerShown: false,
+          headerTitle: (props) => <Header {...props} />,
+          headerTitleAlign: "center",
+          headerShown: true,
           headerBackVisible: false,
+        })}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="Collection"
+        component={CollectionScreen}
+        options={({ navigation }) => ({
+          headerTitle: (props) => <Header {...props} />,
+          headerTitleAlign: "center",
+          headerShown: true,
+          headerBackVisible: false,
+        })}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="ProductDetails"
+        component={MerchDetailsScreen}
+        options={({ navigation }) => ({
+          headerTitle: (props) => <Header {...props} />,
+          headerTitleAlign: "center",
+          headerShown: true,
+          headerBackVisible: false,
+          headerLeft: () => {
+            return <GoBack navigation={navigation} />;
+          },
+        })}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="MusicDetails"
+        component={MusicDetailScreen}
+        options={({ navigation }) => ({
+          headerTitle: (props) => <Header {...props} />,
+          headerTitleAlign: "center",
+          headerShown: true,
+          headerBackVisible: false,
+          headerLeft: () => {
+            return <GoBack navigation={navigation} />;
+          },
+        })}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="Cart"
+        component={CartScreen}
+        options={({ navigation }) => ({
+          headerTitle: (props) => <Header {...props} />,
+          headerTitleAlign: "center",
+          headerShown: true,
+          headerBackVisible: false,
+          headerLeft: () => {
+            return <GoBack navigation={navigation} />;
+          },
         })}
       ></Stack.Screen>
     </Stack.Navigator>
   );
 };
 
-export default MainNavigator;
+export default StoreNavigator;
