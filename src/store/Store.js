df7ca8244b3e27f12c88f3f4942507066eb6d93f
shawn barbel <barbels33@gmail.com>
2022-03-30 20:03:12 -0400

    added reducers

--- src/store/Store.js
+++ src/store/Store.js
@@ -4,12 +4,16 @@ import { songReducer } from "./reducers/songReducer";
 import { PlayerReducer } from "./reducers/PlayerReducer";
 import { beatReducer } from "./reducers/beatReducer";
 import { authReducer } from "./reducers/authReducer";
+import { productsReducer } from "./reducers/productsReducer";
+import { cartReducer } from "./reducers/cartReducer";
 
 const rootReducer = combineReducers({
   songs: songReducer,
   player: PlayerReducer,
   beats: beatReducer,
   auth: authReducer,
+  products: productsReducer,
+  cart: cartReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
