8895c7a5ab782d93d27156f728cf050f5b05dc3c
shawn barbel <barbels33@gmail.com>
2022-04-03 00:23:11 -0400

    created music detail screen

--- src/screens/store/sections/musicDetails/MusicDetailScreen.js
+++ src/screens/store/sections/musicDetails/MusicDetailScreen.js
@@ -1,17 +1,56 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { config } from "../../../../config/Config";
+import { theme } from "../../../../config/Theme";
 
 const MusicDetailScreen = ({ navigation }) => {
+  const selectedProduct = useSelector(
+    (state) => state.products.selectedProduct
+  );
+
   return (
     <View style={styles.container}>
-      <Text>MusicDetailScreen</Text>
+      <ImageBackground
+        source={{ uri: selectedProduct?.image }}
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      ></ImageBackground>
+      <View
+        style={{
+          position: "absolute",
+          backgroundColor: "white",
+          height: config.hp("55%"),
+          width: "100%",
+          bottom: 0,
+          borderTopRightRadius: 25,
+          borderTopLeftRadius: 25,
+          paddingHorizontal: config.wp("4%"),
+          paddingVertical: config.hp("2%"),
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 22,
+            color: theme.colors.primary,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          {selectedProduct?.title}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { padding: 10 },
+  container: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    height: "100%",
+  },
+  backgroundImage: { width: "100%", height: config.hp("45%") },
 });
 
 export default MusicDetailScreen;
