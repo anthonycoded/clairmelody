10f1672cce6057e094fc4d155c6bfb6a32150fca
shawn barbel <barbels33@gmail.com>
2022-04-19 17:41:45 -0400

    updatd music detail screen

--- src/screens/store/sections/musicDetails/MusicDetailScreen.js
+++ src/screens/store/sections/musicDetails/MusicDetailScreen.js
@@ -1,14 +1,27 @@
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { config } from "../../../../config/Config";
 import { theme } from "../../../../config/Theme";
+import { AddToCart } from "../../../../store/actions/cartActions";
 
 const MusicDetailScreen = ({ navigation }) => {
   const selectedProduct = useSelector(
     (state) => state.products.selectedProduct
   );
 
+  const dispatch = useDispatch();
+
+  function addToCart() {
+    dispatch(AddToCart(selectedProduct));
+  }
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -20,7 +33,7 @@ const MusicDetailScreen = ({ navigation }) => {
         style={{
           position: "absolute",
           backgroundColor: "white",
-          height: config.hp("55%"),
+          height: config.hp("65%"),
           width: "100%",
           bottom: 0,
           borderTopRightRadius: 25,
@@ -29,16 +42,100 @@ const MusicDetailScreen = ({ navigation }) => {
           paddingVertical: config.hp("2%"),
         }}
       >
-        <Text
-          style={{
-            fontSize: 22,
-            color: theme.colors.primary,
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
-        >
-          {selectedProduct?.title}
-        </Text>
+        <ScrollView style={{}}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: config.hp("4%"),
+            }}
+          >
+            <View>
+              <Text
+                style={{
+                  fontSize: 22,
+                  color: theme.colors.primary,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  textTransform: "capitalize",
+                }}
+              >
+                {selectedProduct?.title}
+              </Text>
+              <Text
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                style={{ textTransform: "capitalize", fontSize: 16 }}
+              >
+                {selectedProduct?.credits}
+              </Text>
+            </View>
+            <View style={{ alignItems: "flex-end" }}>
+              <Text
+                style={{
+                  fontSize: 22,
+                  color: theme.colors.primary,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}
+              >
+                ${selectedProduct?.leasePrice}/{selectedProduct?.purchasePrice}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: theme.colors.primary,
+                  textAlign: "center",
+                }}
+              >
+                Lease/Purchase
+              </Text>
+            </View>
+          </View>
+
+          <View
+            style={{ justifyContent: "flex-start", height: config.hp("35%") }}
+          >
+            <Text style={{ fontSize: 22 }}>Lease:</Text>
+            <Text style={{ marginBottom: 10 }}>
+              Eiusmod ipsum duis tempor et enim commodo fugiat amet exercitation
+              adipisicing. Laboris qui voluptate ullamco eu reprehenderit in
+              labore elit esse labore do. Eiusmod pariatur nostrud sunt Lorem
+              culpa cillum voluptate fugiat nulla in cillum adipisicing anim
+            </Text>
+            <Text style={{ fontSize: 22 }}>Purchase:</Text>
+            <Text>
+              Eiusmod ipsum duis tempor et enim commodo fugiat amet exercitation
+              adipisicing. Laboris qui voluptate ullamco eu reprehenderit in
+              labore elit esse labore do. Eiusmod pariatur nostrud sunt Lorem
+              culpa cillum voluptate fugiat nulla in cillum adipisicing anim
+            </Text>
+          </View>
+
+          <View
+            style={{
+              marginBottom: 48,
+              alignItems: "center",
+              justifyContent: "flex-start",
+              paddingBottom: config.hp("15%"),
+            }}
+          >
+            <TouchableOpacity
+              onPress={addToCart}
+              style={{
+                paddingVertical: 10,
+                backgroundColor: theme.colors.primary,
+                width: config.wp("50%"),
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 25,
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 18 }}>Add To Cart</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
