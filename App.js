811ead5bd53111526b7e7c3048a586bbedfc1d72
shawn barbel <barbels33@gmail.com>
2022-04-19 17:37:58 -0400

    added stripe provider

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import store from "./src/store/Store";
 import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider } from "react-native-paper";
 import { theme } from "./src/config/Theme";
 import AppWrapper from "./AppWrapper";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 function App() {
   return (
     <Provider store={store}>
       <PaperProvider theme={theme}>
-        <AppWrapper></AppWrapper>
-
+        <StripeProvider publishableKey="pk_test_51IaNZlCILXbg6WUSGDz5uOCrk6AgKamms3Lpa8DtMAdyakKzcelFTqPzbYDU8H9F21ZJimiH55WOROIUp2hae6sa00G0olzXE0">
+          <AppWrapper></AppWrapper>
+        </StripeProvider>
         <StatusBar style="light"></StatusBar>
       </PaperProvider>
     </Provider>
