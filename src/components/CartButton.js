117b82fe6e7124e6b323adf31c893952d142cd2f
shawn barbel <barbels33@gmail.com>
2022-04-19 17:39:21 -0400

    updated styles

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,18 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
 import React from "react";
 import { theme } from "../config/Theme";
+import { CartLength } from "../utils/store/cart";
 
-const CartButton = () => {
-  const cart = useSelector((state) => state.cart.cart);
+const CartButton = ({ navigation }) => {
+  const cartLength = CartLength();
   return (
-    <View style={{ paddingHorizontal: 5 }}>
+    <TouchableOpacity
+      onPress={() => navigation.navigate("Cart")}
+      style={{ paddingHorizontal: 8 }}
+    >
       <Ionicons name="cart" size={32} color={theme.colors.primary} />
-      <Text style={{ position: "absolute", right: 0, top: 0, fontSize: 18 }}>
-        {cart?.length}
+      <Text
+        style={{
+          position: "absolute",
+          right: 0,
+          top: 0,
+          fontSize: 18,
+        }}
+      >
+        {cartLength}
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
